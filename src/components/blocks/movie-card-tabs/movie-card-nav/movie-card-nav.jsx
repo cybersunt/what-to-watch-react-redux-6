99f7f43a2bd41d5dd5306e58395de3af498639d8
Yulia <juliam2007@mail.ru>
2021-03-01 23:06:48 +0300

    какой-то коммит

--- src/components/blocks/movie-card-tabs/movie-card-nav/movie-card-nav.jsx
+++ src/components/blocks/movie-card-tabs/movie-card-nav/movie-card-nav.jsx
@@ -5,7 +5,7 @@ const MovieCardNav = ()=> {
     <nav className="movie-nav movie-card__nav">
       <ul className="movie-nav__list">
         <li className="movie-nav__item movie-nav__item--active">
-          <a href="#" className="movie-nav__link">Overview</a>
+          <a href="#" className="movie-nav__link">Overview88888</a>
         </li>
         <li className="movie-nav__item">
           <a href="#" className="movie-nav__link">Details</a>
