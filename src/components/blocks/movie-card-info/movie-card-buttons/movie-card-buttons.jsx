a61b44ac2547460a93418307587b6ced5a24f4a5
Yulia <juliam2007@mail.ru>
2021-03-27 19:38:14 +0300

    fix: настроен редирект для кнопки "Мой список", если пользовател не авторизован

--- src/components/blocks/movie-card-info/movie-card-buttons/movie-card-buttons.jsx
+++ src/components/blocks/movie-card-info/movie-card-buttons/movie-card-buttons.jsx
@@ -5,9 +5,7 @@ import Link from "../../link/link";
 import {useHistory} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {addMovieMyMovieList} from "../../../../store/user-actions/user-actions-api-action";
-import {AuthorizationStatus, RoutePath} from "../../../../constants/constants";
-
-const STATUS_ADD_MOVIE = 1;
+import {AuthorizationStatus, RoutePath, STATUS_ADD_MOVIE} from "../../../../constants/constants";
 
 const MovieCardButtons = ({fullVersion}) => {
   const history = useHistory();
@@ -17,33 +15,34 @@ const MovieCardButtons = ({fullVersion}) => {
   const {authorizationStatus} = useSelector((state) => state.USER_DATA);
   const dispatch = useDispatch();
 
-  const onButtonPlayClick = () => id ? history.push(`${RoutePath.PLAYER}${id}`) : history.push(`${RoutePath.PLAYER}${promoMovie.id}`);
+  const handleButtonPlayClick = () => id ? history.push(`${RoutePath.PLAYER}${id}`) : history.push(`${RoutePath.PLAYER}${promoMovie.id}`);
 
-  const handleSubmit = () => {
+  const onSubmit = () => {
     return id === undefined ?
       dispatch(addMovieMyMovieList({filmId: promoMovie.id, status: STATUS_ADD_MOVIE})) :
       dispatch(addMovieMyMovieList({filmId: id, status: STATUS_ADD_MOVIE}));
   };
 
+  const handleSubmit = authorizationStatus === AuthorizationStatus.AUTH ? onSubmit : ()=> history.push(RoutePath.LOGIN);
+
   return (
     <div className="movie-card__buttons">
-      <button className="btn btn--play movie-card__button" type="button" onClick={onButtonPlayClick}>
+      <button className="btn btn--play movie-card__button" type="button" onClick={handleButtonPlayClick}>
         <svg viewBox="0 0 19 19" width="19" height="19">
           <use xlinkHref="#play-s"></use>
         </svg>
         <span>Play</span>
       </button>
 
-      {authorizationStatus === AuthorizationStatus.AUTH ? (
-        <button className="btn btn--list movie-card__button" type="button" onClick={handleSubmit}>
-          <svg viewBox="0 0 19 20" width="19" height="20">
-            <use xlinkHref="#add"></use>
-          </svg>
-          <span>My list</span>
-        </button>
-      ) : null}
+      <button className="btn btn--list movie-card__button" type="button" onClick={handleSubmit}>
+        <svg viewBox="0 0 19 20" width="19" height="20">
+          <use xlinkHref="#add"></use>
+        </svg>
+        <span>My list</span>
+      </button>
 
-      {fullVersion && authorizationStatus === AuthorizationStatus.AUTH ? <Link pathName={`${RoutePath.FILMS}${id}/review`} className="btn movie-card__button">Add review</Link> : null}
+      {fullVersion && authorizationStatus === AuthorizationStatus.AUTH ?
+        <Link pathName={`${RoutePath.FILMS}${id}/review`} className="btn movie-card__button">Add review</Link> : null}
     </div>
   );
 };
