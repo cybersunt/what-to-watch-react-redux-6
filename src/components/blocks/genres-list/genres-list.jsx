be525763cec852ca41830338bf73123329d6b72f
Yulia <juliam2007@mail.ru>
2021-03-30 16:58:25 +0300

    refactor: перенос событий из DOM в события реакта

--- src/components/blocks/genres-list/genres-list.jsx
+++ src/components/blocks/genres-list/genres-list.jsx
@@ -4,33 +4,36 @@ import {useDispatch, useSelector} from "react-redux";
 import {getGenresItems} from "../../../utils/utils";
 import {changeGenre, resetFilter} from "../../../store/movies-filter/movies-filter-action";
 import {MAX_DISPLAY_COUNT_GENRES} from "../../../constants/constants";
+import PropTypes from "prop-types";
+
+const GenreItem = ({item}) => {
+  const dispatch = useDispatch();
+  const {currentFilterGenre} = useSelector((state) => state.FILTERS);
+
+  const handleChangeFilter = (evt) => {
+    evt.preventDefault();
+    dispatch(resetFilter());
+    dispatch(changeGenre(item));
+  };
+
+  return (
+    <li className={classnames(`catalog__genres-item`, {[`catalog__genres-item--active`]: currentFilterGenre === item})} onClick={handleChangeFilter}>
+      <a href="#" className="catalog__genres-link">{item}</a>
+    </li>
+  );
+};
+
+GenreItem.propTypes = {item: PropTypes.string.isRequired};
 
 const GenresList = () => {
   const {movies} = useSelector((state) => state.DATA);
-  const {currentFilterGenre} = useSelector((state) => state.FILTERS);
-  const dispatch = useDispatch();
 
   const genresItems = getGenresItems(movies);
   const genresList = genresItems.length > MAX_DISPLAY_COUNT_GENRES ? genresItems.slice(0, Math.min(genresItems.length, MAX_DISPLAY_COUNT_GENRES)) : genresItems;
 
-  const handleChangeFilter = (evt) => {
-    evt.preventDefault();
-    const filterName = evt.target.id;
-    if (evt.target.tagName === `A`) {
-      dispatch(resetFilter());
-      dispatch(changeGenre(filterName));
-    }
-  };
-
   return (
-    <ul className="catalog__genres-list" onClick={handleChangeFilter}>
-      {genresList.map((item, index) => {
-        return (
-          <li key={index} className={classnames(`catalog__genres-item`, {[`catalog__genres-item--active`]: currentFilterGenre === item})}>
-            <a id={item} href="#" className="catalog__genres-link">{item}</a>
-          </li>
-        );
-      })}
+    <ul className="catalog__genres-list">
+      {genresList.map((item) => <GenreItem key={item.toLowerCase()} item={item}/>)}
     </ul>
   );
 };
