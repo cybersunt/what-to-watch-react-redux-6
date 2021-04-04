2c59ce3c4d9b4d2fcde4e4b6307f4d7132bcd764
Yulia <juliam2007@mail.ru>
2021-04-04 23:30:40 +0300

    fix: удален лишний код

--- src/components/pages/player/player.jsx
+++ src/components/pages/player/player.jsx
@@ -1,42 +1,24 @@
-import React, {useEffect} from "react";
+import React from "react";
 import MainLayout from "../../layouts/main-layout/main-layout";
 import VideoPlayer from "../../sections/video-player/video-player";
 import MoviesList from "../../blocks/movies-list/movies-list";
 import Loader from "../../blocks/loader/loader";
 import {useHistory} from "react-router-dom";
-import {PROMO_MOVIE_ID, RoutePath} from "../../../constants/constants";
-import {useDispatch, useSelector} from "react-redux";
-import {resetCurrentMovie, resetPromoMovie} from "../../../store/movie-data/movie-data-action";
-import {fetchCurrentMovie, fetchPromoMovie} from "../../../store/movie-data/movie-data-api-actions";
+import {RoutePath} from "../../../constants/constants";
+import {useSelector} from "react-redux";
+import useLoadedMovie from "../../../hooks/use-loaded-movie";
 
 const Player = ({id}) => {
   const history = useHistory();
-  const dispatch = useDispatch();
+  const [currentMovie, isCurrentMovieLoaded] = useLoadedMovie(id);
   const {promoMovie, isPromoMovieLoaded} = useSelector((state) => state.DATA_ITEM);
-  const {currentMovie, isCurrentMovieLoaded} = useSelector((state) => state.DATA_ITEM);
-  const videoId = id === undefined ? PROMO_MOVIE_ID : id;
-  const actualMovie = videoId === PROMO_MOVIE_ID ? promoMovie : currentMovie;
-  const {videoLink} = actualMovie;
+  const videoLink = id === promoMovie.id ? promoMovie.videoLink : currentMovie.videoLink;
+  const videoId = id === promoMovie.id ? promoMovie.id : currentMovie.id;
 
-  const historyPath = ()=> {
+  const handleButtonExitClick = ()=> {
     return history.action === `PUSH` ?
       history.goBack() :
       history.push(`${RoutePath.FILMS}${videoId}`);
-  };
-
-  useEffect(() => {
-    if (!isPromoMovieLoaded || id === undefined) {
-      dispatch(fetchPromoMovie());
-    }
-    if (!isCurrentMovieLoaded || id !== currentMovie.id) {
-      dispatch(fetchCurrentMovie(id));
-    }
-  }, [id, isCurrentMovieLoaded, isPromoMovieLoaded]);
-
-  const handleButtonExitClick = ()=> {
-    dispatch(resetCurrentMovie());
-    dispatch(resetPromoMovie());
-    historyPath();
   };
 
   return (
